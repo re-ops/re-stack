67149bda396f42e3ed7603575a8da08be06b2e53
ronen <narkisr.com>
2018-12-04 17:57:49 +1100

    update stack and status

--- src/re_stack/cloudformation.clj
+++ src/re_stack/cloudformation.clj
@@ -7,8 +7,20 @@
 (defn create-stack [stack template capabilities]
   (form/create-stack aws :stack-name stack :template-url template :capabilities capabilities))
 
+(defn update-stack [stack template capabilities]
+  (form/update-stack aws :stack-name stack :template-url template :capabilities capabilities))
+
 (defn delete-stack [stack]
   (form/delete-stack aws :stack-name stack))
 
 (defn describe-stack [stack]
   (form/describe-stack-resources aws :stack-name stack))
+
+(defn list-stasks []
+  (:stack-summaries (form/list-stacks aws)))
+
+(defn stack-status
+  "Tasks with name prefix status"
+  [prefix]
+  (map (juxt :stack-name :stack-status)
+       (filter (fn [{:keys [stack-name]}] (.startsWith stack-name prefix)) (list-stasks))))
